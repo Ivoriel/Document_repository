ffd410a429429137d527cde63370b44833371555
Ivor <tkivor@gmail.com>
2023-10-09 23:12:19 +0200

    Added main class

---
+++ src/main/java/pl/tkosinski/documentrepository/DocumentRepository.java
@@ -0,0 +1,13 @@
+package pl.tkosinski.documentrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentRepository {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentRepository.class, args);
+	}
+
+}
